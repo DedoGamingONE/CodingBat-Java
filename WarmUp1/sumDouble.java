e02becd514255ae85fba60c8169ebf1a42269103
VictorMOchoa <ochoa.m.victor@gmail.com>
2016-05-18 18:57:21 -0700

    Deleted duplicate files/weird thing that happened.

--- WarmUp1/sumDouble.java
+++
@@ -1,6 +0,0 @@
-public class sumDouble {
-	/*	Given two int values, return their sum. Unless the two values are the same, then return double their sum.*/
-	public int sumDouble(int a, int b) {
-		  return a != b ? (a + b) : (2*(a + b));
-	}
-}
