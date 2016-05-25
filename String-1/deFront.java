dbc71fdfb891f5394c23407f766669ec4b8bc3a2
VictorMOchoa <ochoa.m.victor@gmail.com>
2016-05-25 14:57:24 -0700

    Only one String1 problem remaining

--- String-1/deFront.java
+++ String-1/deFront.java
@@ -3,6 +3,27 @@ public class deFront {
 	/*Given a string, return a version without the first 2 chars. Except keep the first char if it is 'a' and keep the 
 	second char if it is 'b'. The string may be any length. Harder than it looks.*/
 	public String deFront(String str) {    
-		  
+		if(str.length() < 2) {
+			return str;
+		}
+		
+		StringBuilder builder = new StringBuilder();
+		
+		if(str.charAt(0) != 'a') {
+			
+		} else {
+			builder.append(str.charAt(0));
+		}
+		
+		if(str.charAt(1) != 'b') {
+			
+		} else {
+			builder.append(str.charAt(1));
+		}
+		
+		if(str.length() > 2) {
+			builder.append(str.substring(2));
+		}
+		return builder.toString();
 	}
 }
--- String-1/withoutX.java
+++ String-1/withoutX.java
@@ -3,7 +3,31 @@ public class withoutX {
 	//Given a string, if the first or last chars are 'x', return the string without those 'x' chars, and otherwise 
 	//return the string unchanged.
 	public String withoutX(String str) {
-		  
+		StringBuilder builder = new StringBuilder();
+		if(str.length() <= 1) {
+			if(str.equals("x")) {
+				return "";
+			}
+			return str;
+		} else if(str.length() == 2) {
+			if(str.charAt(0) != 'x') {
+				builder.append(str.charAt(0));
+			} 
+			if(str.charAt(1) != 'x') {
+				builder.append(str.charAt(1));
+			}
+		} else {
+			if(str.charAt(0) != 'x') {
+				builder.append(str.charAt(0));
+			} 
+			
+			builder.append(str.substring(1, str.length()-1));
+			
+			if(str.charAt((str.length()-1)) != 'x') {
+				builder.append(str.substring(str.length()-1));
+			}
+		}
+		return builder.toString();
 	}
 
 	
