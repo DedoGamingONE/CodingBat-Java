e23641d65d759042c05bb5d7d27dcd5091edbb90
VictorMOchoa <ochoa.m.victor@gmail.com>
2016-05-29 18:59:16 -0700

    First half of Array-1 Problems completely solved.

---
+++ Array-1/makeEnds.java
@@ -0,0 +1,11 @@
+
+public class makeEnds {
+	/*Given an array of ints, return a new array length 2 containing the first and last elements from the 
+	 * original array. The original array will be length 1 or more.*/
+	public int[] makeEnds(int[] nums) {
+		int[] newArr = new int[2];
+		newArr[0] = nums[0];
+		newArr[1] = nums[nums.length-1];
+		return newArr;
+	}
+}
---
+++ Array-1/middleWay.java
@@ -0,0 +1,10 @@
+
+public class middleWay {
+	/*Given 2 int arrays, a and b, each length 3, return a new array length 2 containing their middle elements.*/
+	public int[] middleWay(int[] a, int[] b) {
+		int[] newArr = new int[2];
+		newArr[0] = a[1];
+		newArr[1] = b[1];
+		return newArr;
+	}
+}
