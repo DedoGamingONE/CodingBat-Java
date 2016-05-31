feb2373d2142d73cbea44bf921a88463fab0b0d4
VictorMOchoa <ochoa.m.victor@gmail.com>
2016-05-31 16:11:18 -0700

    ~1/3

---
+++ Logic-1/squirrelPlay.java
@@ -0,0 +1,19 @@
+
+public class squirrelPlay {
+	/*The squirrels in Palo Alto spend most of the day playing. In particular, they play if the temperature is between 60 and 90 
+	 * (inclusive). Unless it is summer, then the upper limit is 100 instead of 90. Given an int temperature and a boolean isSummer, 
+	 * return true if the squirrels play and false otherwise.*/
+	public boolean squirrelPlay(int temp, boolean isSummer) {
+		  if(isSummer) {
+			  if(temp <= 100 && temp >= 60) {
+				  return true;
+			  }
+		  } else {
+			  if(temp <= 90 && temp >= 60) {
+				  return true;
+			  }
+		  }
+		  return false;
+	}
+
+}
