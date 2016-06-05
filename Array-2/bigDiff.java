c05c0fc9be158064c9af1e84cb36f9369aef7028
VictorMOchoa <ochoa.m.victor@gmail.com>
2016-06-05 13:54:30 -0700

    First commit of the day.

---
+++ Array-2/bigDiff.java
@@ -0,0 +1,16 @@
+
+public class bigDiff {
+	/*Given an array length 1 or more of ints, return the difference between the largest and smallest values in the 
+	 * array. Note: the built-in Math.min(v1, v2) and Math.max(v1, v2) methods return the smaller or larger of two values.*/
+	public int bigDiff(int[] nums) {
+		int largest = nums[0];
+		int smallest = nums[0];
+		for(int i = 0; i < nums.length; i++) {
+			largest = Math.max(largest, nums[i]);
+			smallest = Math.min(smallest, nums[i]);
+		}
+		
+		return Math.abs(largest-smallest);
+	}
+
+}
