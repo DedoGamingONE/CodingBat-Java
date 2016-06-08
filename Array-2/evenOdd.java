a03d5f70dc74b8a9ff9f666b38e9097462cd2cb2
Victor Ochoa <ochoa.m.victor@gmail.com>
2016-06-07 19:41:13 -0700

    All Array-2 problems complete

--- Array-2/evenOdd.java
+++ Array-2/evenOdd.java
@@ -4,7 +4,24 @@ public class evenOdd {
 	 * the even numbers come before all the odd numbers. Other than that, the numbers can be in any order. 
 	 * You may modify and return the given array, or make a new array.*/
 	public int[] evenOdd(int[] nums) {
-		  
+		int evenCount = 0;
+		int position = 0;
+		int[] result = new int[nums.length];
+		for(int i = 0; i < nums.length; i++) {
+			if(nums[i] % 2 == 0) {
+				evenCount++;
+				result[position] = nums[i];
+				position++;
+			}
+		}
+
+		for(int i = 0; i < nums.length; i++) {
+			if(nums[i] % 2 != 0) {
+				result[position] = nums[i];
+				position++;
+			}
+		}
+		return result;
 	}
 
 }
