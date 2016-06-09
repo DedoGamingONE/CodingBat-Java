ce01660a766f7ce10ab4c42cff6c5584b59b9759
Victor Ochoa <ochoa.m.victor@gmail.com>
2016-06-08 22:01:11 -0700

    Start of Recursion-1 Problems

---
+++ Recursion-1/factorial.java
@@ -0,0 +1,11 @@
+
+public class factorial {
+	/*Given n of 1 or more, return the factorial of n, which is n * (n-1) * (n-2) ... 1. Compute the 
+	 * result recursively (without loops).*/
+	public int factorial(int n) {
+		  if(n == 0)
+		    return 1;
+		  return n * factorial(n-1);
+		}
+
+}
