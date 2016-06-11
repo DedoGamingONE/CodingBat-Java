7219f30162433b858f24f35ec4c36cd47214c941
Victor Ochoa <ochoa.m.victor@gmail.com>
2016-06-11 13:13:14 -0700

    Started Map-1 Problems

---
+++ Map-1/mapBully.java
@@ -0,0 +1,14 @@
+import java.util.Map;
+
+public class mapBully {
+	/*Modify and return the given map as follows: if the key "a" has a value, set the key "b" to have that value, 
+	and set the key "a" to have the value "". Basically "b" is a bully, taking the value of "a".*/
+	public Map<String, String> mapBully(Map<String, String> map) {
+		  if(map.containsKey("a")) {
+		  map.put("b", map.get("a"));
+		  map.put("a", "");
+		  }
+		  return map;
+		}
+
+}
