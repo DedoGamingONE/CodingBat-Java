e4b4f0d30967fa9c819e79bfc0c21be5b238ee6d
unknown <ochoa.m.victor@gmail.com>
2020-06-09 20:43:52 -0700

    Begin Map2 problemset

---
+++ Map-2/word0.java
@@ -0,0 +1,14 @@
+import java.util.Map;
+
+public class word0 {
+  public Map<String, Integer> word0(String[] strings) {
+    Map<String, Integer> map = new HashMap();
+    for (int i = 0; i < strings.length; i++) {
+      if (!map.containsKey(strings[i])) {
+        map.put(strings[i], 0);
+      }
+
+    }
+    return map;
+  }
+}
