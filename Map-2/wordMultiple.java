f51093cb10f523913e96d10313615e635d884a62
Victor Ochoa <ochoa.m.victor@gmail.com>
2020-06-15 10:02:37 -0700

    More map questions

---
+++ Map-2/wordMultiple.java
@@ -0,0 +1,20 @@
+import java.util.Map;
+
+public class wordMultiple {
+  /*
+  Given an array of strings, return a Map<String, Boolean> where each different
+  string is a key and its value is true if that string appears 2 or more times
+  in the array.
+  */
+  public Map<String, Boolean> wordMultiple(String[] strings) {
+    Map<String, Boolean> map = new HashMap();
+
+    for (int i = 0; i < strings.length; i++) {
+      if (map.containsKey(strings[i])) {
+        map.put(strings[i], true);
+      }else {
+        map.put(strings[i], false);
+      }
+    }
+    return map;
+  }
